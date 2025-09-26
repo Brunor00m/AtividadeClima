c16c57ffbb93755fbff64b5a9bdfd156811992c0
DEV_Romão <brunodossantosromao11@gmail.com>
2025-09-26 15:13:53 -0300

    js atualizado

--- ProjetoClima/script.js
+++ ProjetoClima/script.js
@@ -1,48 +1,22 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    const form = document.querySelector('#form');
-    const input = document.querySelector('#input');
-    const resultado = document.querySelector('#resultado');
-    const titulo = document.querySelector('#titulo');
-    const tempInfo = document.querySelector('.tempInfo');
-    const ventoInfo = document.querySelector('.ventoInfo');
-    const img = document.querySelector('.temp img');
-    const aviso = document.querySelector('#aviso');
-    const ventoPonto = document.querySelector('.ventoPonto div');
+  let input = document.querySelector('#searchInput').value;
 
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      let cidade = input.value.trim();
-      if (cidade !== '') {
-        showWarning('Carregando...');
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=SUA_API_KEY&units=metric&lang=pt_br`;
-        let results = await fetch(url);
-        let json = await results.json();
-        if (json.cod === 200) {
-          showInfo({
-            name: json.name,
-            country: json.sys.country,
-            temp: json.main.temp,
-            tempIcon: json.weather[0].icon,
-            windSpeed: json.wind.speed,
-            windAngle: json.wind.deg
-          });
-        } else {
-          showWarning('Não encontramos esta cidade.');
-        }
-      }
-    });
+  if (input !== '') {
+    showWarning('Carregando...');
 
-    function showInfo(json) {
-      showWarning('');
-      resultado.style.display = 'block';
-      titulo.innerHTML = `${json.name}, ${json.country}`;
-      tempInfo.innerHTML = `${json.temp}<sup>ºC</sup>`;
-      ventoInfo.innerHTML = `${json.windSpeed}<span> km/h</span>`;
-      img.setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
-      ventoPonto.style.transform = `rotate(${json.windAngle - 90}deg)`;
-    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=bf06888ce7a545492d64642bb7984471&units=metric&lang=pt_br`;
 
-    function showWarning(msg) {
-      aviso.innerHTML = msg;
-    }
-  
+    let result = await fetch(url);
+    let json = await result.json();
+
+    console.log(json);
+  } else {
+    showWarning('Digite uma cidade válida!');
+  }
+});
+
+function showWarning(msg) {
+  document.querySelector('.aviso').innerHTML = msg;
+}
